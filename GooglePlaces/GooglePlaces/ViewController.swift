f78a84f9c7ac9bcda461969cb3a70af8781891ab
Cesar Alejandro Guadarrama Ortega <cesar@MacBook-Air-de-Cesar.local>
2023-11-17 12:16:46 -0600

    Archivo de Xcode creado

---
+++ GooglePlaces/GooglePlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GooglePlaces
+//
+//  Created by Cesar Alejandro Guadarrama Ortega on 17/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
